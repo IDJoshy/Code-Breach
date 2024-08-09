baa5ce014e3531216f25367eb2b743bac317122b
EphemeralJosh <ephemeralstudiounity@gmail.com>
2024-08-09 18:08:33 -0400

    NavBar_Update (Added letter animation to hamburger menu)

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,5 @@
+// Hamburger Menu
+
 const hamburger__menu = document.querySelector(".navbar__hamburger__menu");
 
 const hamburger_button = document.querySelector(".navbar__hamburger__button");
@@ -35,3 +37,141 @@ function UnToggleMenu()
     hamburger_icon_close.classList.add("navbar__hamburger__button__icons--hidden");
     hamburger__menu.classList.add("navbar__hamburger__menu--hidden");
 }
+
+// hamburger menu animations
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+
+const hamburger_link = document.querySelectorAll(".navbar__hamburger__list__links--link");
+
+hamburger_link[0].onmouseover = event =>
+{
+    let iterations = 0;
+
+    const interval = setInterval(() =>
+    {
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => 
+        {
+            if(index < iterations)
+            {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 27)]
+        })
+        .join("");   
+
+        if(iterations >= 9) 
+        {
+            clearInterval(interval);
+        }
+        
+        iterations += 1;
+    }, 30);
+}
+
+hamburger_link[1].onmouseover = event =>
+{
+    let iterations = 0;
+
+    const interval = setInterval(() =>
+    {
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => 
+        {
+            if(index < iterations)
+            {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 27)]
+        })
+        .join("");   
+
+        if(iterations >= event.target.dataset.value.length) 
+        {
+            clearInterval(interval);
+        }
+        
+        iterations += 1;
+    }, 30);
+}
+
+hamburger_link[2].onmouseover = event =>
+{
+    let iterations = 0;
+
+    const interval = setInterval(() =>
+    {
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => 
+        {
+            if(index < iterations)
+            {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 27)]
+        })
+        .join("");   
+
+        if(iterations >= event.target.dataset.value.length) 
+        {
+            clearInterval(interval);
+        }
+        
+        iterations += 1;
+    }, 30);
+}
+
+hamburger_link[3].onmouseover = event =>
+{
+    let iterations = 0;
+
+    const interval = setInterval(() =>
+    {
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => 
+        {
+            if(index < iterations)
+            {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 27)]
+        })
+        .join("");   
+
+        if(iterations >= event.target.dataset.value.length) 
+        {
+            clearInterval(interval);
+        }
+        
+        iterations += 1;
+    }, 30);
+}
+
+hamburger_link[4].onmouseover = event =>
+{
+    let iterations = 0;
+
+    const interval = setInterval(() =>
+    {
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => 
+        {
+            if(index < iterations)
+            {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 27)]
+        })
+        .join("");   
+
+        if(iterations >= event.target.dataset.value.length) 
+        {
+            clearInterval(interval);
+        }
+        
+        iterations += 1;
+    }, 30);
+}
+
+// Hamburger Menu End
